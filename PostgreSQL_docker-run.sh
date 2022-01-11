6beaaa2e33fc4ab4f80a511bd60b016ee9cf9f8d
sebssekk <sebssekk@hotmail.it>
2022-01-11 17:21:13 +0100

    fix

--- PostgreSQL_docker-run.sh
+++ PostgreSQL_docker-run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 docker run --name psql -p 5432:5432  -e POSTGRES_PASSWORD=testPsw -e POSTGRES_USER=testUser -e POSTGRES_DB=student -d postgres
 
-## Per accedere successivamente 
+########### Per acedere
 #docker exec -it psql psql student testuser
 
+########
 
 
 
